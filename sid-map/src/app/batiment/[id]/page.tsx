881c38729630fda4d36cd27af04b2842e85f9d89
Loup0007A <lr000000007@gmail.com>
2026-08-28 12:25:25 +0200

    Delete sid-map directory

--- sid-map/src/app/batiment/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-import BuildingClient from '@/components/building/BuildingClient';
-
-export default function BatimentPage({ params }: { params: { id: string } }) {
-  return <BuildingClient buildingId={params.id} />;
-}
--- sid-map/src/app/ville/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-import CityMap from '@/components/city/CityMap';
-
-export default function VillePage({ params }: { params: { id: string } }) {
-  return <CityMap cityId={params.id} />;
-}
